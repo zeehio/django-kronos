f22f638120920b722edd7619d6a037289edc678e
Sergio Oller <sergioller@gmail.com>
2015-03-25 01:54:25 +0100

    Modifications in METASHARE-3.0.1. PROJECT_MODULE has to be 'metashare' instead of 'metashare.settings'. Needed to prevent errors in Metashare node syncing

--- kronos/settings.py
+++ kronos/settings.py
@@ -6,4 +6,4 @@ from django.conf import settings
 KRONOS_PYTHON = getattr(settings, 'KRONOS_PYTHON', sys.executable)
 KRONOS_MANAGE = getattr(settings, 'KRONOS_MANAGE', '%s/manage.py' % os.getcwd())
 KRONOS_PYTHONPATH = getattr(settings, 'KRONOS_PYTHONPATH', None)
-PROJECT_MODULE = sys.modules['.'.join(settings.SETTINGS_MODULE.split('.')[:-1])]
+PROJECT_MODULE = sys.modules['.'.join(settings.SETTINGS_MODULE.split('.'))]
